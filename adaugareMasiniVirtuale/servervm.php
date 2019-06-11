f15aa286d0f4de2313470f20b405475933f8fd5c
IrinaApostol <48645231+IrinaApostol@users.noreply.github.com>
2019-06-12 01:19:04 +0300

    Add files via upload

---
+++ adaugareMasiniVirtuale/servervm.php
@@ -0,0 +1,63 @@
+<?php
+session_start();
+
+// initializing variables
+	
+	$numevm ="";
+    $ip="";
+    $os="";
+    $useracc="";
+	$errors = array(); 
+
+// connect to the database
+	$db = mysqli_connect('localhost', 'root', '', 'penguinix');
+	if(isset($_POST['addmachine'])){
+		//receive all input values from the form
+		$numevm = mysqli_real_escape_string($db,$_POST['namevm']);
+		$ip = mysqli_real_escape_string($db,$_POST['ip']);
+        $os = mysqli_real_escape_string($db,$_POST['os']);
+        $useracc=mysqli_real_escape_string($db,$_POST['useraccess']);
+   // form validation: ensure that the form is correctly filled ...
+// by adding (array_push()) corresponding error unto $errors array
+		if(empty($numevm)) { array_push($errors,"Trebuie sa introduceti numele masinii virtuale.");}
+		if(empty($ip)) { array_push($errors,"Trebuie sa introduceti adresa ip.");}
+		if(empty($os)) { array_push($errors,"Trebuie sa introduceti sistemul de operare.");}
+        if(empty($useracc)) { array_push($errors,"Trebuie sa introduceti numele utilizatorului care are acces.");}
+		//first check the database to make sure
+		//a user does not already exist with the same username
+        $vm_check_query = "SELECT * FROM virtualmachine WHERE name='$numevm' LIMIT 1";
+		$result = mysqli_query ($db , $vm_check_query);
+		$vm = mysqli_fetch_array($result, MYSQLI_ASSOC);
+		if($vm){ //if vm exists
+			if($vm['name']== $numevm){
+				array_push($errors, "Aceasta masina virtuala exista deja");}
+			}
+			
+		//Finally , register user is there are no errors in the form
+		if(!count($errors)){
+
+			$query = "select id from users where username='$useracc'";
+			$res = mysqli_query($db,$query);
+			$arr = mysqli_fetch_array($res , MYSQLI_ASSOC);
+			$idacc = $arr['id'];
+			$query = "select idmachine from virtualmachine where name= '$numevm'";
+			$res = mysqli_query($db,$query);
+			$arr1 = mysqli_fetch_array($res,MYSQLI_ASSOC);
+			$idvm = $arr1['idmachine'];
+
+			if($idacc==NULL)
+			array_push($errors,"Nu exista acest utilizator");
+			else
+			{
+
+			$query1="INSERT INTO virtualmachine (name,ip,os)
+					VALUES ('$numevm','$ip','$os')";
+			mysqli_query($db, $query1);
+			
+
+			$query = "INSERT INTO useraccess values ($idacc,$idvm)";
+			mysqli_query($db,$query);	
+			$success_message = "<center>Ati adaugat masina virtuala cu succes!";
+		}
+	}
+}
---
+++ adaugareMasiniVirtuale/vm.php
@@ -0,0 +1,38 @@
+<?php include("servervm.php"); ?>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" href="register.css">
+</head>
+<body background="ptTW.png">
+        <br/>
+        <h1>penGUIniX Virtual Machine</h1>
+<div class="register">
+    <br/>
+        <img src="pencil-icon.png" alt="pencil-icon" style="float:left;width: 50px;height: 50px; ">
+        <br/> <br/> <br/> <br/>
+        <?php include("erori.php"); ?>
+        <?php if(!empty($success_message)) { ?>	
+		<div class="success"><?php if(isset($success_message)) echo $success_message; ?>
+        </div>
+		<?php } ?>
+   <form method="POST" action="vm.php">
+        <input type="text"placeholder="Name" name="namevm">
+    <br/> <br/>
+        <input type="text"placeholder="IP" name="ip">
+    <br/> <br/>
+        <input type="text"
+        placeholder="OS" name="os">
+        <br/> <br/>
+        <input type="text"
+        placeholder="Nume utilizator acces" name="useraccess">
+        <br/> <br/>
+        <input type="password"
+        placeholder="Root password" name="root">
+        <br/> <br/>
+                <input type="submit" value="Add Machine" name="addmachine" onclick="window.location.href='file:///C:/Users/Denisa%20Irina/Desktop/TW/PROIECT/virtualmachine.html'" />
+                <!-- <button class="buton2">Register</button> -->
+    </form>
+</div>
+</body>
+</html>
